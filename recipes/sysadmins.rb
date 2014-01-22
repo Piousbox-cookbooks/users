e3ca612856872282fb6e3061f27f75b9bb23f83a
Victor Piousbox <piousbox@gmail.com>
2014-01-22 09:15:38 -0800

    no action on user sysadmin.

--- recipes/sysadmins.rb
+++ recipes/sysadmins.rb
@@ -20,7 +20,10 @@
 
 # Searches data bag "users" for groups attribute "sysadmin".
 # Places returned users in Unix group "sysadmin" with GID 2300.
-users_manage "sysadmin" do
-  group_id 2300
-  action [ :remove, :create ]
-end
+
+# _vp_ 20140122 - AWS Ubuntu has no sysadmin user, and I don't use it anywhere.
+#
+#users_manage "sysadmin" do
+#  group_id 2300
+#  action [ :remove, :create ]
+#end
